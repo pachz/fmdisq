3ee306ee872263f0f23ae9e151e2b5ee4bbe0eba
Payam Chamani <pchamani@nizek.com>
2025-04-06 23:23:06 +0330

    fix

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-require('dotenv').config();
-const sql = require('mssql');
+require("dotenv").config();
+const sql = require("mssql");
 
-const policyIds = [
-    '3BD25C81-B7DF-428C-83AE-3CA92F722DC7',
-    'C8D7B75F-D6BB-443F-ACEA-CB588F55150B',
-    'B3D8C596-67DC-4533-8748-731871EB262C'
-  ];
+const policies = {
+    paaq1: "3BD25C81-B7DF-428C-83AE-3CA92F722DC7",
+    paaq2: "C8D7B75F-D6BB-443F-ACEA-CB588F55150B",
+    paaq3: "B3D8C596-67DC-4533-8748-731871EB262C",
+}
+
+const policyIds = Object.values(policies);
 
 // Config from .env
 const config = {
@@ -15,9 +17,43 @@ const config = {
   database: process.env.DB_NAME,
   options: {
     encrypt: true,
-    trustServerCertificate: true
-  }
+    trustServerCertificate: true,
+  },
 };
+
+async function insertRecord(code, Msidn) {
+  try {
+    // await sql.connect(config);
+    const request = new sql.Request();
+
+    request.input("DiscountCode", sql.VarChar, code);
+    request.input("Msidn", sql.BigInt, Msidn);
+    request.input("DiscountPolicyId", sql.UniqueIdentifier, policies[code]);
+    request.input("IsDeleted", sql.Bit, 0);
+    request.input(
+      "CreatedAt",
+      sql.DateTimeOffset,
+      "2020-01-01 12:00:00 +00:00"
+    );
+
+    const insertQuery = `
+        INSERT INTO dbo.UsedDiscounts (
+            Id, DiscountCode, Msidn, DiscountPolicyId,
+            IsDeleted, SeqId, CreatedAt
+        ) VALUES (
+            NEWID(), @DiscountCode, @Msidn, @DiscountPolicyId,
+            @IsDeleted, @CreatedAt
+        )
+    `;
+
+    await request.query(insertQuery);
+    console.log("Record insert successful.", code, Msidn);
+  } catch (err) {
+    console.error("Insert failed:", err);
+  } finally {
+    // await sql.close();
+  }
+}
 
 async function runQuery() {
   try {
@@ -30,23 +66,30 @@ async function runQuery() {
     });
 
     // Inject the policy count
-    request.input('policyCount', sql.Int, policyIds.length);
+    request.input("policyCount", sql.Int, policyIds.length);
 
-    const whereClause = policyIds.map((_, index) => `@id${index}`).join(', ');
+    const whereClause = policyIds.map((_, index) => `@id${index}`).join(", ");
 
     const query = `
-      SELECT TOP 1 Msidn
+      SELECT TOP 1 Msisdn
       FROM dbo.UsedDiscounts
       WHERE DiscountPolicyId IN (${whereClause}) AND IsDeleted = 0
-      GROUP BY Msidn
+      GROUP BY Msisdn
       HAVING COUNT(DISTINCT DiscountPolicyId) < @policyCount
     `;
 
     const result = await request.query(query);
+    console.log("Result:", result.recordset);
 
-    console.log('Result:', result.recordset);
+    if (result?.recordset?.length > 0) {
+        const Msisdn = result?.recordset[0].Msisdn;
+        for (const code in policies) {
+            await insertRecord(code, Msisdn);
+        }
+    }
+
   } catch (err) {
-    console.error('Error running query:', err);
+    console.error("Error running query:", err);
   } finally {
     await sql.close();
   }
